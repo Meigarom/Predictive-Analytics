bdd756a1750efdd7a9ac3aa28d4c7efef493022c
meigarom <meigaromlopes@gmail.com>
2019-09-10 22:06:25 -0300

    finish bot

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,15 @@
 from tokens import cmc_token
+from flask import Flask, request, Response
 
 import requests
+import json
+import re
+
+token = '923271662:AAGhKPqrlbYWnMgfPYm_zr_NsL140_fL3Gs'
+
+
+app = Flask( __name__ )
+
 
 def write_json( data, filename='response.json' ):
     with open( filename, 'w' ) as f:
@@ -19,10 +28,50 @@ def get_cmc_data( crypto ):
     return price
 
 
+def parse_message(message):
+    chat_id = message['message']['chat']['id']
+    txt = message['message']['text']
 
-def main():
-    print( get_cmc_data( 'BTC' ) )
+    pattern = r'/[a-zA-Z]{2,4}'
+
+    ticker = re.findall( pattern, txt ) # [...]
+
+    if ticker:
+        symbol = ticker[0][1:].upper()   # /btc > btc  .strip('/')
+    else:
+        symbol = ''
+
+    return chat_id, symbol
+
+
+def send_message( chat_id, text='bla-bla-bla' ):
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    payload = {'chat_id': chat_id, 'text': text}
+
+    r = requests.post( url, json=payload )
+    return r
+
+
+
+@app.route( '/', methods=['POST', 'GET'] )
+def index():
+    if request.method == 'POST':
+        msg = request.get_json()
+        chat_id, symbol = parse_message( msg )
+
+        if not symbol:
+            send_message( chat_id, 'Wrong data' )
+            return Response( 'Ok', status=200 )
+
+        price = get_cmc_data( symbol )
+        send_message( chat_id, price )
+
+        return Response( 'Ok', status=200 ) 
+
+    else:
+        return '<h1>Coin Market Cap bot </h1>'
+
 
 
 if __name__ == '__main__':
-    main()
+    app.run( debug=True )
