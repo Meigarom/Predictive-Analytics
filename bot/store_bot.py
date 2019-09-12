2c12145bb45b283320762421aab15b71e87a968b
meigarom <meigaromlopes@gmail.com>
2019-09-11 23:29:39 -0300

    implement the store bot

--- bot/store_bot.py
+++ bot/store_bot.py
@@ -1,9 +1,11 @@
 import os
+import re
 import json
 import pandas as pd
 import requests
 
 from configparser import ConfigParser
+from flask import Flask, request, Response
 
 config = ConfigParser()
 config.read( os.environ['HOME'] + '/' + '.config.ini' )
@@ -11,18 +13,8 @@ config.read( os.environ['HOME'] + '/' + '.config.ini' )
 ROOT_PATH = config['ROSSMANN']['ROOT_PATH']
 TOKEN = config['ROSSMANN']['TOKEN']
 
-# original api
-#https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/getMe
 
-# replace token
-#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/getMe
-
-# endpoint to receive a message from Telegram 
-#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/getUpdates
-
-# endpoint to send a message to Telegram 
-#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/sendMessage?chat_id=449124440&text=Hello User
-#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/sendMessage?chat_id=449124440&text=Store 1 will sell $183,801.81 in the next 6 weeks
+app = Flask( __name__ )
 
 
 def get_data():
@@ -43,7 +35,7 @@ def get_data():
 
     return d
 
-def get_prediction(store):
+def get_prediction( store ):
     url = 'http://0.0.0.0:5000/rossmann/predict'
     h = {'Content-type': 'application/json'}
     d = get_data()
@@ -57,10 +49,80 @@ def get_prediction(store):
 
     return s
 
+
+def send_message( chat_id, text='kkkk' ):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {'chat_id': chat_id, 'text': text }
+
+    r = requests.post( url, json=payload )
+    return r
+
+
+def parse_message( message ):
+    chat_id = message['message']['chat']['id']
+    txt = message['message']['text'] # /btc or /maid
+
+    #pattern = r'/[a-zA-Z]{2,4}'
+
+    #ticker = re.findall( pattern, txt )
+
+    symbol = int( txt.replace( '/', '' ) )
+
+    #if ticker:
+    #    symbol = ticker[0][1:]
+    #else:
+    #    symbol = ''
+
+    return chat_id, symbol
+
+
+
+@app.route( '/', methods=['POST', 'GET'] )
+def index():
+    if request.method == 'POST':
+        msg = request.get_json()
+        chat_id, symbol = parse_message( msg )
+
+        if not symbol:
+            send_message( chat_id, 'Wrong Data' )
+            return Response( 'Ok', status=200 )
+
+        pred = get_prediction( store=symbol )
+
+        store_id = pred['Store'].values[0] 
+        value = pred['Prediction'].values[0]
+        m = 'Store {} will sell ${:,.2f} in the next 6 weeks'.format( store_id, value ) 
+
+        send_message( chat_id, m  )
+
+        return Response( 'Ok', status=200 )
+         
+    else:
+        return '<h1>Store Bot</h1>'
+
 def main():
     p = get_prediction( store=1 )
 
     print( 'Store {} will sell ${:,.2f} in the next 6 weeks'.format( p['Store'][0], p['Prediction'][0] ) )
 
 if __name__ == '__main__':
-    main()
+    app.run( port=5001, debug=True )
+
+# original api
+#https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/getMe
+
+# replace token
+#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/getMe
+
+# endpoint to receive a message from Telegram 
+#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/getUpdates
+
+# endpoint to send a message to Telegram 
+#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/sendMessage?chat_id=449124440&text=Hello User
+
+#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/sendMessage?chat_id=449124440&text=Store 1 will sell $183,801.81 in the next 6 weeks
+
+# set the Webhook
+#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/setWebhook?url=https://arcus.serveo.net/
+
+#https://api.telegram.org/bot937473363:AAFipUhaNR042WusW5i2e1Ibh4e4ZFMh8so/setWebhook?url=https://meigarom-ux5n.localhost.run
